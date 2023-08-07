97336bc2752f1a5d5f3af220bb7d47308257c7d0
thiha129 <71546593+thiha129@users.noreply.github.com>
2023-08-07 15:16:54 +0700

    code UI ProductComponent

--- src/pages/ProductList/AsideFilter/AsideFilter.tsx
+++ src/pages/ProductList/AsideFilter/AsideFilter.tsx
@@ -95,8 +95,8 @@ export default function AsideFilter() {
                 <svg viewBox='0 0 9.5 8' className='w-4 h-4 mr-1' key={index}>
                   <defs>
                     <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset='{0}' stopColor='#ffca11' />
-                      <stop offset='{1}' stopColor='#ffad27' />
+                      <stop offset='0' stopColor='#ffca11' />
+                      <stop offset='1' stopColor='#ffad27' />
                     </linearGradient>
                     <polygon
                       id='ratingStar'
@@ -129,8 +129,8 @@ export default function AsideFilter() {
                 <svg viewBox='0 0 9.5 8' className='w-4 h-4 mr-1' key={index}>
                   <defs>
                     <linearGradient id='ratingStarGradient' x1='50%' x2='50%' y1='0%' y2='100%'>
-                      <stop offset='{0}' stopColor='#ffca11' />
-                      <stop offset='{1}' stopColor='#ffad27' />
+                      <stop offset='0' stopColor='#ffca11' />
+                      <stop offset='1' stopColor='#ffad27' />
                     </linearGradient>
                     <polygon
                       id='ratingStar'
