05030abf438db72e599d50606a4bbc425bffcfec
thiha129 <thim00549@gmail.com>
2024-03-08 10:11:27 +0700

    UI ProductDetail

--- src/useRouterElements.tsx
+++ src/useRouterElements.tsx
@@ -8,6 +8,7 @@ import Profile from './pages/Profile'
 import { useContext } from 'react'
 import { AppContext } from './contexts/app.context'
 import path from './constants/path'
+import ProductDetail from './pages/ProductDetail'
 
 // const isAuthenticated = true
 function ProtectedRoute() {
@@ -27,6 +28,15 @@ export default function useRouterElements() {
       element: (
         <MainLayout>
           <ProductList />
+        </MainLayout>
+      )
+    },
+    {
+      path: path.productDetail,
+      index: true,
+      element: (
+        <MainLayout>
+          <ProductDetail />
         </MainLayout>
       )
     },
