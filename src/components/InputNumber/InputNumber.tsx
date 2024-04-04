39008346d01f2cf684fa6744920a6674204e4985
thiha129 <thim00549@gmail.com>
2024-04-04 10:12:44 +0700

    update InputNumber and QuantityController

--- src/components/InputNumber/InputNumber.tsx
+++ src/components/InputNumber/InputNumber.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, forwardRef, useState } from 'react'
 export interface InputNumberProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
   classNameInput?: string
@@ -13,20 +13,25 @@ const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(function Inpu
     classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm shadow-sm',
     classNameError = 'nt-1 text-red-600 min-h-[1.25rem] text-sm',
     onChange,
+    value = '',
     ...rest
   },
   ref
 ) {
+  const [localvalue, setLocalvalue] = useState<string>(value as string)
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
-    if ((/^\d+$/.test(value) || value === '') && onChange) {
-      onChange(event)
+    if (/^\d+$/.test(value) || value === '') {
+      // Thực thi onChange callback từ bên ngoài truyền vào props
+      onChange && onChange(event)
+      // Cập nhật locaValue state
+      setLocalvalue(value)
     }
   }
 
   return (
     <div className={className}>
-      <input className={classNameInput} onChange={handleChange} {...rest} {...ref} />
+      <input className={classNameInput} onChange={handleChange} value={value || localvalue} {...rest} {...ref} />
       <div className={classNameError}>{errorMessage}</div>
     </div>
   )
