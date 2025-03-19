7a45281ae7dbeae513b238bfe5f2c573d1af9d58
thiha129 <thim00549@gmail.com>
2025-03-19 15:49:44 +0700

    Code UI page profile

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,5 +1,75 @@
-import React from 'react'
+import Input from 'src/components/Input'
 
 export default function Profile() {
-  return <div>Profile</div>
+  return (
+    <div className='rounded-sm bg-white px-2 md:px-7 pb-10 md:pb-20 shadow'>
+      <div className='border-b border-b-gray-200 py-6'>
+        <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ sơ của tôi </h1>
+        <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
+      </div>
+      <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+        <form className='mt-6 flex-grow md:pr-12 md:mt-0'>
+          <div className='flex flex-wrap flex-col sm:flex-row'>
+            <div className='sm:w-[20%] truncate pt-3 sm:text-right capitalize'>Email</div>
+            <div className='sm:w-[80%] sm:sm:pl-5'>
+              <div className='pt-3 text-gray-700'>th*******@gmail.com</div>
+            </div>
+          </div>
+          <div className='mt-6 flex flex-wrap flex-col sm:flex-row '>
+            <div className='sm:w-[20%] truncate pt-3 sm:text-right capitalize'>Tên</div>
+            <div className='w-[80%] sm:pl-5'>
+              <Input classNameInput='px-3 py-2 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap flex-col sm:flex-row '>
+            <div className='sm:w-[20%] truncate pt-3 sm:text-right capitalize'>Số điện thoại</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='px-3 py-2 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap flex-col sm:flex-row '>
+            <div className='sm:w-[20%] truncate pt-3 sm:text-right capitalize'>Địa chỉ</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <Input classNameInput='px-3 py-2 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm shadow-sm' />
+            </div>
+          </div>
+          <div className='mt-2 flex flex-wrap flex-col sm:flex-row '>
+            <div className='w-[20%] truncate pt-3 sm:text-right capitalize'>Ngày sinh</div>
+            <div className='sm:w-[80%] sm:pl-5'>
+              <div className='flex justify-between'>
+                <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Ngày</option>
+                </select>
+                <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Tháng</option>
+                </select>
+                <select name='' id='' className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
+                  <option disabled>Năm</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </form>
+        <div className='flex justify-center md:w-72 md:border-l border-l-gray-200 '>
+          <div className='flex flex-col items-center'>
+            <div className='my-5 h-24 w-24'>
+              <img
+                src='https://down-vn.img.susercontent.com/file/vn-11134004-7ra0g-m7g9jmhdglq466_tn'
+                alt=''
+                className='w-full h-full rounded-full object-cover'
+              />
+            </div>
+            <input className='hidden' type='file' accept='.jpg,.jpeg,.png' />
+            <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              Chọn ảnh
+            </button>
+            <div className='mt-3  text-gray-400'>
+              <div>Dụng lượng file tối đa 1 MB</div>
+              <div>Định dạng:.JPEG, .PNG</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
