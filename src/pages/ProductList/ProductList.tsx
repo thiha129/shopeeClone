f384839595ec4786fa9de285b91731218a5ab802
thiha129 <thim00549@gmail.com>
2025-05-05 17:31:24 +0700

    plus

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,26 +1,23 @@
-import AsideFilter from './components/AsideFilter'
-import SortProductList from './components/SortProductList'
-import Product from './components/Product'
 import { useQuery } from '@tanstack/react-query'
+import { Helmet } from 'react-helmet-async'
+import categoryApi from 'src/apis/category.api'
 import productApi from 'src/apis/product.api'
 import Pagination from 'src/components/Pagination'
-import { ProductListConfig } from 'src/types/product.type'
-import categoryApi from 'src/apis/category.api'
 import useQueryConfig from 'src/hooks/useQueryConfig'
-
-export type QueryConfig = {
-  [key in keyof ProductListConfig]: string
-}
+import { ProductListConfig } from 'src/types/product.type'
+import AsideFilter from './components/AsideFilter'
+import Product from './components/Product/Product'
+import SortProductList from './components/SortProductList'
 
 export default function ProductList() {
   const queryConfig = useQueryConfig()
 
   const { data: productsData } = useQuery({
-    queryKey: ['product', queryConfig],
+    queryKey: ['products', queryConfig],
     queryFn: () => {
       return productApi.getProducts(queryConfig as ProductListConfig)
     },
-    keepPreviousData: true,
+    placeholderData: (prev) => prev,
     staleTime: 3 * 60 * 1000
   })
 
@@ -30,8 +27,13 @@ export default function ProductList() {
       return categoryApi.getCategories()
     }
   })
+
   return (
     <div className='bg-gray-200 py-6'>
+      <Helmet>
+        <title>Trang chủ | Shopee Clone</title>
+        <meta name='description' content='Trang chủ dự án Shopee Clone' />
+      </Helmet>
       <div className='container'>
         {productsData && (
           <div className='grid grid-cols-12 gap-6'>
@@ -40,7 +42,7 @@ export default function ProductList() {
             </div>
             <div className='col-span-9'>
               <SortProductList queryConfig={queryConfig} pageSize={productsData.data.data.pagination.page_size} />
-              <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
+              <div className='mt-6 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
                 {productsData.data.data.products.map((product) => (
                   <div className='col-span-1' key={product._id}>
                     <Product product={product} />
